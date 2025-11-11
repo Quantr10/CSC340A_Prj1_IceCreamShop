3bb0c333204f0ba6618ede3c91c95edbfd3bfd4c
Quan Tran <thequantran2005@gmail.com>
2025-11-11 05:22:21 -0500

    add rating UI and rating logic

--- src/components/IceCreamCard.vue
+++ src/components/IceCreamCard.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="card menu-card h-100 border-0 shadow-sm">
-    <img
-      :src="image"
-      :alt="name"
-      class="card-img-top menu-img"
-    />
+    <img :src="image" :alt="name" class="card-img-top menu-img" />
 
     <div class="card-body text-center d-flex flex-column">
       <h5 class="card-title menu-name">{{ name }}</h5>
-      <p class="menu-rating mt-auto">⭐ {{ rating }}/5</p>
+
+      <div class="menu-stars">
+        <font-awesome-icon icon="star" class="star-icon" />
+        <span class="rating-number">{{ computedRating }}/5</span>
+      </div>
+
       <p class="menu-price">${{ price }}</p>
       <button class="menu-btn mt-auto" @click="onRate">Rate This</button>
     </div>
@@ -16,14 +17,34 @@
 </template>
 
 <script setup>
-defineProps({
+import { useRouter } from "vue-router";
+import { computed } from "vue";
+import "../assets/IceCreamCard.css";
+
+const router = useRouter();
+
+const props = defineProps({
   image: String,
   name: String,
   price: String,
   rating: String,
-})
+});
+
+const computedRating = computed(() => {
+  const stored = JSON.parse(localStorage.getItem("ratings") || "{}");
+  const data = stored[props.name];
+  return data ? data.avg.toFixed(1) : props.rating;
+});
 
 const onRate = () => {
-  alert(`You rated ${name}! 🍦`)
-}
+  router.push({
+    name: "Rating",
+    params: { name: props.name },
+    query: {
+      image: props.image,
+      price: props.price,
+      rating: props.rating,
+    },
+  });
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,28 +1,38 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-// Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css'
-// Bootstrap JS (optional, for interactive components)
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// Font Awesome Core
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-// Solid icons
-import { faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+  faLocationDot,
+  faPhone,
+  faEnvelope,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
-// Vue Font Awesome component
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-// Add all icons you need to the library
-library.add(faLocationDot, faPhone, faEnvelope, faFacebookF, faInstagram, faTwitter)
+library.add(
+  faLocationDot,
+  faPhone,
+  faEnvelope,
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+  faStar
+);
 
-// Create app
-const app = createApp(App)
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(router);
+app.mount("#app");
